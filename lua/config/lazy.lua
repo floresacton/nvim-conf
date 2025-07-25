ccb2d2556b7425aeae11651ec285204b8a4f4673
Miguel Flores-Acton <floresacton@gmail.com>
2025-07-25 01:03:04 -0400

    Save tabbing config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,12 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 
+-- Setup indenting
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
